2617f522802664c416210c3db0a4b26d3d6b3489
Pillay, Anil (Tax&Accounting) <anil.pillay@thomsonreuters.com>
2016-01-15 17:55:38 -0400

    Add Custom attribute to use in XML and in code using attrs.xml

--- NewDraw/app/src/main/java/com/ondraw/apillay/newdraw/Pizza.java
+++ NewDraw/app/src/main/java/com/ondraw/apillay/newdraw/Pizza.java
@@ -1,6 +1,7 @@
 package com.ondraw.apillay.newdraw;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -9,34 +10,50 @@ import android.view.View;
 
 /**
  * Created by apillay on 1/15/2016.
+ * custom attributes are defined in attrs.xml and can be used in code and uin layout xml
+ * for the custom attribute to work in xml we have to add the namespace to the xml file
+ * xmlns:anyname="http://schemas.android.com/apk/res-auto" eg
+ * xmlns:pizza="http://schemas.android.com/apk/res-auto"
  */
 public class Pizza extends View {
+    // Avoid creating instances in onDraw
     private Paint paint;
+    //private final int NUM_EDGES = 5;
+    private int numWedges = 5;
     public Pizza(Context context) {
         super(context);
-        init();
+
+        init(context, null);
     }
 
     public Pizza(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
+        init(context, attrs);
     }
 
     public Pizza(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
+        init(context, attrs);
     }
 
     public Pizza(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-        init();
+        init(context, attrs);
     }
 
-    private void init() {
+    private void init(Context context, AttributeSet attrs) {
+        int strokeWidth = 4;
+        int color = Color.YELLOW;
+        if (attrs != null) {
+            TypedArray aList = context.obtainStyledAttributes(attrs, R.styleable.Pizza);
+            strokeWidth = aList.getDimensionPixelSize(R.styleable.Pizza_stroke_width,strokeWidth);
+            color = aList.getColor(R.styleable.Pizza_color, color);
+            numWedges = aList.getInt(R.styleable.Pizza_num_wedges, numWedges);
+        }
         paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(4);
-        paint.setColor(Color.YELLOW);
+        paint.setStrokeWidth(strokeWidth);
+        paint.setColor(color);
     }
     @Override
     protected void onDraw(Canvas canvas) {
@@ -47,5 +64,17 @@ public class Pizza extends View {
         final float diameter = Math.min(width,height) - paint.getStrokeWidth();
         final float radius = diameter/2;
         canvas.drawCircle(cx,cy, radius, paint);
+        drawPizzaCut(canvas,cx,cy,radius);
+    }
+
+    // rotate pizza to simplify the cut
+    private void drawPizzaCut(Canvas canvas, float x, float y, float radius) {
+        final float degrees = 360f / numWedges;
+        canvas.save();
+        for (int i=0; i<numWedges;i++) {
+            canvas.drawLine(x, y, x, y - radius, paint);
+            canvas.rotate(degrees, x, y); // rotate the whole canvas
+        }
+        canvas.restore();  // undos any canvas rotation or transformation but not the drawing.
     }
 }
